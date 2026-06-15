091c8181efa21a7dc56da07be9bf9fbf24d47933
Victalejo <victalejodev@gmail.com>
2026-06-14 20:13:05 -0500

    Initial Commit

---
+++ Argos/ArgosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArgosApp.swift
+//  Argos
+//
+//  Created by Victor Alejandro Cano Jaramillo on 14/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ArgosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
